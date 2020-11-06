79389210548208ce0952915962a06abd26455923
hi <kangja1996@gmail.com>
2020-11-05 20:53:00 -0500

    progress

---
+++ introduction-test-driven-development.js
@@ -0,0 +1,11 @@
+// Test-Driven Development or TDD is a software development paradigm that reverses the notion that you should first develop code and then test it. Instead in TDD, you write your test and then you write code.
+
+// We will follow Red/Green/Refactor TDD methodlogy. 
+
+// Red - Write a test. The test fails because you haven't written any code yet. 
+
+// Green - Write code that passes your test (and all previous tests). Don't be clever, just write code so your tests pass!
+
+// Refactor - Refactor your code! there are many reasons to refactor, such as efficiency, code style, and readibility. Make sure your code still passes your tests as you refactor. You repeat the cycle until your functionality is done. 
+
+// We're going to use the popular JEST testing package.
